aef9bf4d3e7ea8eebf2ff5f38215d6ce17118998
narutosakata <safalbahare.199719@gmail.com>
2017-12-03 17:08:11 +0530

    Updated Code for LOGIN,LOGOUT,Permissions

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -7,3 +7,20 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(
+        required = True,
+        label = 'Username',
+        max_length = 32
+    )
+    email = forms.CharField(
+        required = True,
+        label = 'Email',
+        max_length = 32,
+    )
+    password = forms.CharField(
+        required = True,
+        label = 'Password',
+        max_length = 32,
+        widget = forms.PasswordInput()
+    )
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
-from .forms import PostForm
+from .forms import PostForm,UserRegistrationForm
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django import forms
+from django.contrib import messages
 # Create your views here.
 
 def post_list(request):
@@ -27,7 +32,13 @@ def post_new(request):
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+    if(post.author!=request.user):
+       #response=HttpResponse("You don't have the permission")
+       #response.status_code=403
+       #return response
+        messages.error(request,"PermissionDenied")
+        return redirect('post_detail',pk=post.pk)
+    elif request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
@@ -38,3 +49,25 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            userObj = form.cleaned_data
+            username = userObj['username']
+            email =  userObj['email']
+            password =  userObj['password']
+            if not (User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists()):
+                User.objects.create_user(username, email, password)
+                user = authenticate(username = username, password = password)
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                return render(request, 'blog/register.html', {'form' : form})
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form' : form})
+
